d9b935c25a7811af0161d9b6ded0f3334b4f491c
weidongkl <weidongkl@sina.com>
2022-09-06 07:46:29 +0800

    optimistic format of log
    Signed-off-by: weidongkl <weidongkl@sina.com>

--- internal/logger/formatter.go
+++ internal/logger/formatter.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	defaultLogFormat       = "[%levelname%] %time%  [%filename%:%lineno%]: %message%\n"
+	defaultLogFormat       = "[%levelname%] %time%  [%filename%:%lineno%]: %extends% %message%\n"
 	defaultTimestampFormat = "2006-01-02 15:04:05"
 )
 
@@ -36,5 +36,19 @@ func (m PotatoFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 
 	output = strings.Replace(output, "%lineno%", strconv.Itoa(entry.Caller.Line), 1)
 	output = strings.Replace(output, "%filename%", entry.Caller.File, 1)
+	for k, val := range entry.Data {
+
+		switch v := val.(type) {
+		case string:
+			output = strings.Replace(output, "%extends%", k+"="+v+" "+"%extends%", 1)
+		case int:
+			s := strconv.Itoa(v)
+			output = strings.Replace(output, "%extends%", k+"="+s+" "+"%extends%", 1)
+		case bool:
+			s := strconv.FormatBool(v)
+			output = strings.Replace(output, "%extends%", k+"="+s+" "+"%extends%", 1)
+		}
+	}
+	output = strings.Replace(output, "%extends%", "", 1)
 	return []byte(output), nil
 }
