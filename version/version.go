1d89bf1fc97b2fd860c0b17d34215f200d879997
weidongkl <weidongkl@sina.com>
2022-10-05 23:46:17 +0800

    增加版本信息

--- version/version.go
+++ version/version.go
@@ -3,17 +3,12 @@ package version
 import "fmt"
 
 var (
-	// VersionMajor is for an API incompatible changes
-	VersionMajor = 0
-	// VersionMinor is for functionality in a backwards-compatible manner
-	VersionMinor = 1
-	// VersionPatch is for backwards-compatible bug fixes
-	VersionPatch = 0
-
-	// VersionDev indicates development branch. Releases will be empty string.
-	VersionDev = "dev"
+	Major        = 0
+	Minor        = 1
+	Micro        = 0
+	ReleaseLevel = "dev"
 )
 
 // Version is the specification version that the package types support.
 var Version = fmt.Sprintf("%d.%d.%d+%s",
-	VersionMajor, VersionMinor, VersionPatch, VersionDev)
+	Major, Minor, Micro, ReleaseLevel)
